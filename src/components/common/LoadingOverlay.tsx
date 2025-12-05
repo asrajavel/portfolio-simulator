1163735e03ae48d89a1b381e1853cb154b881c83
asrajavel <asrajavel267@gmail.com>
2025-12-05 12:08:30 +0530

    fix spinner

--- src/components/common/LoadingOverlay.tsx
+++ src/components/common/LoadingOverlay.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Block } from 'baseui/block';
+import { Spinner } from 'baseui/spinner';
+import { LabelMedium } from 'baseui/typography';
 
 interface LoadingOverlayProps {
   active: boolean;
@@ -13,15 +15,23 @@ export const LoadingOverlay: React.FC<LoadingOverlayProps> = ({ active }) => (
       left="0"
       width="100%"
       height="100%"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
       overrides={{
         Block: {
           style: {
             zIndex: 10,
-            background: 'rgba(255,255,255,0.2)',
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
             pointerEvents: 'all'
           }
         }
       }}
-    />
+    >
+      <Block display="flex" flexDirection="column" alignItems="center" gridGap="scale400">
+        <Spinner />
+        <LabelMedium>Loading...</LabelMedium>
+      </Block>
+    </Block>
   ) : null
 ); 
--- src/components/layout/ChartArea.tsx
+++ src/components/layout/ChartArea.tsx
@@ -3,7 +3,6 @@ import { MultiFundCharts } from '../charts/MultiFundCharts';
 import { mfapiMutualFund } from '../../types/mfapiMutualFund';
 import { SipStrategy } from '../../types/sipStrategy';
 import { LumpsumStrategy } from '../../types/lumpsumStrategy';
-import { LoadingSpinner } from '../common/LoadingSpinner';
 import { Block } from 'baseui/block';
 import { LabelMedium } from 'baseui/typography';
 
@@ -64,29 +63,7 @@ export const ChartArea: React.FC<ChartAreaProps> = ({
       maxWidth="90%"
       margin="0 auto"
     >
-      {(loadingNav || loadingXirr) ? (
-        <Block
-          position="absolute"
-          top="0"
-          left="0"
-          right="0"
-          bottom="0"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          overrides={{
-            Block: {
-              style: {
-                zIndex: 2,
-                background: 'rgba(255,255,255,0.0)'
-              }
-            }
-          }}
-        >
-          <LoadingSpinner text="Loading..." />
-        </Block>
-      ) : (
-        hasPlotted && Object.keys(navDatas).length > 0 && (
+      {hasPlotted && Object.keys(navDatas).length > 0 && (
         <MultiFundCharts
           navDatas={navDatas}
           lumpsumStrategyXirrData={lumpsumStrategyXirrData}
@@ -100,7 +77,6 @@ export const ChartArea: React.FC<ChartAreaProps> = ({
           chartView={chartView}
           isLumpsum={isLumpsum}
         />
-        )
       )}
     </Block>
   </>
